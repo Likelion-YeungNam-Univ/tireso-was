d65e395027c9babcde60c3ce8641b7a4b40d9fe2
daone <jcn07253@gmail.com>
2023-07-31 22:01:48 +0900

    feat: 세팅

---
+++ src/main/java/com/nes/tireso/TiresoApplication.java
@@ -0,0 +1,13 @@
+package com.nes.tireso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiresoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiresoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nes/tireso/TiresoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nes.tireso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiresoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
