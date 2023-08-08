9ece6d6c686dc541c27749ea07346b04ba99b0ed
daone <jcn07253@gmail.com>
2023-08-08 14:50:03 +0900

    feat: Member 관련 클래스 추가
    Entity에서 providerTypeCode는 어떤 소셜 로그인인지 알기 위해서 추가하였고, username은 ex. Naver_123345 이런식으로 저장됨

--- src/main/java/com/nes/tireso/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/nes/tireso/boundedContext/member/controller/MemberController.java
@@ -1,4 +1,32 @@
 package com.nes.tireso.boundedContext.member.controller;
 
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.nes.tireso.boundedContext.member.service.MemberService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/usr/member")
+@RequiredArgsConstructor
 public class MemberController {
+	private final MemberService memberService;
+
+	@PreAuthorize("isAnonymous()")
+	@GetMapping("/login")
+	public String showLogin() {
+		return "usr/member/login";
+	}
+
+	@PreAuthorize("isAuthenticated()")
+	@GetMapping("/me")
+	public String showMe(Model model) {
+
+		return "usr/member/me";
+	}
 }
