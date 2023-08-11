0f5d1ad7e1e3f8a2b45fae38c44d09f9c3a37bc0
daone <jcn07253@gmail.com>
2023-08-11 16:24:08 +0900

    feat: 맴버 엔티티 수정 #6

--- src/main/java/com/nes/tireso/boundedContext/member/entity/Member.java
+++ src/main/java/com/nes/tireso/boundedContext/member/entity/Member.java
@@ -1,19 +1,15 @@
 package com.nes.tireso.boundedContext.member.entity;
 
-import static jakarta.persistence.GenerationType.*;
-
-import com.nes.tireso.base.baseEntity.BaseEntity;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import com.nes.tireso.base.baseEntity.BaseEntity;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -25,9 +21,6 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 @ToString(callSuper = true)
 public class Member extends BaseEntity {
-	@Id
-	@GeneratedValue(strategy = IDENTITY)
-	private Long id;
 	private String providerTypeCode;
 	@Column(unique = true)
 	private String username;
