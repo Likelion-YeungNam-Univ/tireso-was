7a71ddabacca91eccab9ba220d7eeb02938abdc8
daone <jcn07253@gmail.com>
2023-08-11 16:05:50 +0900

    feat: 필요없는 거 삭제 #6

--- src/main/java/com/nes/tireso/boundedContext/member/entity/Member.java
+++ src/main/java/com/nes/tireso/boundedContext/member/entity/Member.java
@@ -1,19 +1,15 @@
 package com.nes.tireso.boundedContext.member.entity;
 
-import static jakarta.persistence.GenerationType.*;
-
-import com.nes.tireso.base.baseEntity.BaseEntity;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import com.nes.tireso.base.baseEntity.BaseEntity;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
