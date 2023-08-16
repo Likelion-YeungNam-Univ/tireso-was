8ac0c714144812924848c3b7e3a472af93e60267
daone <jcn07253@gmail.com>
2023-08-17 05:05:38 +0900

    chore: 스웨거 재설정 #20

--- src/main/java/com/nes/tireso/base/swagger/SwaggerConfig.java
+++ src/main/java/com/nes/tireso/base/swagger/SwaggerConfig.java
@@ -1,30 +1,24 @@
 package com.nes.tireso.base.swagger;
 
-import org.springdoc.core.models.GroupedOpenApi;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.servers.Server;
-
-@PropertySource("classpath:application.yml")
-@OpenAPIDefinition(
-		info = @Info(title = "TIRE,SO. WEB",
-				description = "TIRE,SO. WEB API 명세",
-				version = "v1")
-)
 @Configuration
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
 
-	@Bean
-	public GroupedOpenApi OpenApi() {
-		String[] paths = {"/**"};
-
-		return GroupedOpenApi.builder()
-				.group("TIRE,SO. API v1")
-				.pathsToMatch(paths)
-				.build();
-	}
+    private Info apiInfo() {
+        return new Info()
+                .title("Springdoc 테스트")
+                .description("Springdoc을 사용한 Swagger UI 테스트")
+                .version("1.0.0");
+    }
 }
