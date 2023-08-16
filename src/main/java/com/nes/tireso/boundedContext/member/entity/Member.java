c9229cdbc01e8a2034e2e063c43e2c63fb3b1bf8
daone <jcn07253@gmail.com>
2023-08-16 18:14:08 +0900

    feat: 닉네임, 이메일, 프로필 사진 Entity 추가

--- src/main/java/com/nes/tireso/boundedContext/member/entity/Member.java
+++ src/main/java/com/nes/tireso/boundedContext/member/entity/Member.java
@@ -1,13 +1,6 @@
 package com.nes.tireso.boundedContext.member.entity;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
 import com.nes.tireso.base.baseEntity.BaseEntity;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.Builder;
@@ -15,6 +8,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -22,42 +20,70 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 @ToString(callSuper = true)
 public class Member extends BaseEntity {
-	private String providerTypeCode;
-	@Column(unique = true)
-	private String username;
-	private String name;
-	private String password;
-	private String carType;
-	private String type;
-	private String width;
-	private String ratio;
+    private String providerTypeCode;
+    @Column(unique = true)
+    private String username;
+    private String nickname;
+    private String password;
+    private String email;
+    private String profileImage;
+    private String carType;
+    private String type;
+    private String width;
+    private String ratio;
 
-	public List<? extends GrantedAuthority> getGrantedAuthorities() {
-		List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+    public List<? extends GrantedAuthority> getGrantedAuthorities() {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
 
-		grantedAuthorities.add(new SimpleGrantedAuthority("member"));
+        grantedAuthorities.add(new SimpleGrantedAuthority("member"));
 
-		if (isAdmin()) {
-			grantedAuthorities.add(new SimpleGrantedAuthority("admin"));
-		}
+        if (isAdmin()) {
+            grantedAuthorities.add(new SimpleGrantedAuthority("admin"));
+        }
 
-		return grantedAuthorities;
-	}
+        return grantedAuthorities;
+    }
 
-	public boolean isAdmin() {
-		return "admin".equals(username);
-	}
+    public boolean isAdmin() {
+        return "admin".equals(username);
+    }
 
-	@Builder
-	public Member(String providerTypeCode, String username, String name, String password, String carType, String type,
-			String width, String ratio) {
-		this.providerTypeCode = providerTypeCode;
-		this.username = username;
-		this.name = name;
-		this.password = password;
-		this.carType = carType;
-		this.type = type;
-		this.width = width;
-		this.ratio = ratio;
-	}
+    @Builder
+    public Member(String providerTypeCode, String username, String nickname, String password, String email,
+                  String profileImage, String carType, String type, String width, String ratio) {
+        this.providerTypeCode = providerTypeCode;
+        this.username = username;
+        this.nickname = nickname;
+        this.password = password;
+        this.email = email;
+        this.profileImage = profileImage;
+        this.carType = carType;
+        this.type = type;
+        this.width = width;
+        this.ratio = ratio;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void setProfileImage(String profileImage) {
+        this.profileImage = profileImage;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getProfileImage() {
+        return profileImage;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
