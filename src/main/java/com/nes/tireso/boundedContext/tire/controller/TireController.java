25758c5f3bc518c9aa3b0725e79ee7363b7dc381
Jinwoo Lee <binaryWooo@gmail.com>
2023-08-18 03:06:54 +0900

    feat: tire 필터링 구현

--- src/main/java/com/nes/tireso/boundedContext/tire/controller/TireController.java
+++ src/main/java/com/nes/tireso/boundedContext/tire/controller/TireController.java
@@ -25,8 +25,15 @@ public class TireController {
 
 	@GetMapping
 	@Operation(summary = "타이어 목록 전체 조회 API", description = "타이어 전체 목록을 배열 형태로 반환합니다.")
-	public ResponseEntity<List<Tire>> list(@RequestParam(defaultValue = "0") int sort) {
+	public ResponseEntity<List<Tire>> list(
+			@RequestParam(defaultValue = "0") int sort,
+			@RequestParam(name = "brand_id", defaultValue = "전체") String brandId,
+			@RequestParam(name = "car_type", defaultValue = "전체") String carType,
+			@RequestParam(defaultValue = "전체") String season,
+			@RequestParam(defaultValue = "전체") String type
+	) {
+
 		// 정렬: 등록순, 인기순, 낮은가격순, 높은가격순
-		return new ResponseEntity<>(tireService.list(sort), HttpStatus.OK);
+		return new ResponseEntity<>(tireService.list(sort, brandId, carType, season, type), HttpStatus.OK);
 	}
 }
--- src/main/java/com/nes/tireso/boundedContext/tire/service/TireService.java
+++ src/main/java/com/nes/tireso/boundedContext/tire/service/TireService.java
@@ -15,16 +15,36 @@ import lombok.RequiredArgsConstructor;
 public class TireService {
 	private final TireRepository tireRepository;
 
-	public List<Tire> list(int sort) {
+	public List<Tire> list(int sort, String brandId, String carType, String season, String type) {
+		List<Tire> tireList = null;
+
 		// 정렬: 등록순(0), 인기순(1), 낮은가격순(2), 높은가격순(3)
 		if (sort == 0 || sort > 3) {
-			return tireRepository.findAll(Sort.by(Sort.Direction.ASC, "id"));
+			tireList = tireRepository.findAll(Sort.by(Sort.Direction.ASC, "id"));
 		} else if (sort == 1) {
-			return tireRepository.findAll(Sort.by(Sort.Direction.DESC, "rate"));
+			tireList = tireRepository.findAll(Sort.by(Sort.Direction.DESC, "rate"));
 		} else if (sort == 2) {
-			return tireRepository.findAll(Sort.by(Sort.Direction.ASC, "price"));
+			tireList = tireRepository.findAll(Sort.by(Sort.Direction.ASC, "price"));
 		} else {
-			return tireRepository.findAll(Sort.by(Sort.Direction.DESC, "price"));
+			tireList = tireRepository.findAll(Sort.by(Sort.Direction.DESC, "price"));
 		}
+
+		if (!brandId.equals("전체")) {
+			tireList = tireList.stream().filter(tire -> tire.getBrand().getId().equals(brandId)).toList();
+		}
+
+		if (!carType.equals("전체")) {
+			tireList = tireList.stream().filter(tire -> tire.getCarType().equals(carType)).toList();
+		}
+
+		if (!season.equals("전체")) {
+			tireList = tireList.stream().filter(tire -> tire.getSeason().equals(season)).toList();
+		}
+
+		if (!type.equals("전체")) {
+			tireList = tireList.stream().filter(tire -> tire.getType().equals(type)).toList();
+		}
+
+		return tireList;
 	}
 }
